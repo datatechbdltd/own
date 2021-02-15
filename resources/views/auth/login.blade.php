b839e015b0b73539e2330ea6334863df589cd802
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-02-15 10:51:45 +0600

    Template

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.auth.app')
+@push('title')
+
+@endpush
+@push('meta-description')
+
+@endpush
+@push('meta-image')
+
+@endpush
+@push('style')
+
+@endpush
+@push('food')
+
+@endpush
+@section('content')
+<div class="auth-box login-box">
+    <!-- Start row -->
+    <div class="row no-gutters align-items-center justify-content-center">
+        <!-- Start col -->
+        <div class="col-md-6 col-lg-5">
+            <div class="auth-box-left">
+                <div class="card">
+                    <div class="card-body">
+                        <h4>Your comminuty awaits.</h4>
+                        <div class="auth-box-icon">
+                            <img src="assets/panel/horizontal/images/authentication/auth-box-icon.svg" class="img-fluid" alt="auth-box-icon">
+                        </div>
+                        <div class="auth-box-logo">
+                            <img src="assets/panel/horizontal/images/logo.svg" class="img-fluid " alt="logo">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Start end -->
+        <!-- Start col -->
+        <div class="col-md-6 col-lg-5">
+            <!-- Start Auth Box -->
+            <div class="auth-box-right">
+                <div class="card">
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <h4 class="text-primary mb-4">{{ __('Log in !') }}</h4>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="{{ __('Enter email here') }}" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="password" name="password" placeholder="{{ __('Enter password here') }}" required>
+                            </div>
+                            <div class="form-row mb-3">
+                                <div class="col-sm-6">
+                                    <div class="custom-control custom-checkbox">
+                                      <input type="checkbox" class="custom-control-input" id="rememberme" name="remember">
+                                      <label class="custom-control-label font-14" for="rememberme">{{ __('Remember Me') }}</label>
+                                    </div>
+                                </div>
+                                @if (Route::has('password.request'))
+                                <div class="col-sm-6">
+                                  <div class="forgot-psw">
+                                    <a id="forgot-psw" href="{{url('/template/panel-horizontal/user-forgotpsw')}}" class="font-14">{{ __('Forgot Password?') }}</a>
+                                  </div>
+                                </div>
+                                @endif
+                            </div>
+                          <button type="submit" class="btn btn-success btn-lg btn-block font-18">{{ __('Log in Now') }}</button>
+                        </form>
+                        <div class="login-or">
+                            <h6 class="text-muted">{{ __('OR') }}</h6>
+                        </div>
+                        <div class="social-login text-center">
+                            <button type="submit" class="btn btn-primary-rgba btn-lg btn-block font-18"><i class="mdi mdi-facebook mr-2"></i>{{ __('Log in with Facebook') }}</button>
+                            <button type="submit" class="btn btn-danger-rgba btn-lg btn-block font-18"><i class="mdi mdi-google mr-2"></i>{{ __('Log in with Google') }}</button>
+                        </div>
+                        <p class="mb-0 mt-3"> {{ __('Don\'t have a account?') }} <a href="{{ url('/template/panel-horizontal/user-register') }}">{{ __('Sign up') }}</a></p>
+                    </div>
+                </div>
+            </div>
+            <!-- End Auth Box -->
+        </div>
+        <!-- End col -->
+    </div>
+    <!-- End row -->
+</div>
+@endsection
---
+++ resources/views/layouts/auth/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Theta is a premium responsive admin dashboard template with great features.">
+    <meta name="keywords" content="responsive, admin template, dashboard template, bootstrap 4, laravel, ui kits, ecommerce, web app, crm, cms, html, sass support, scss">
+    <meta name="author" content="Themesbox">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Theta - Login</title>
+    <!-- Fevicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/panel/horizontal/images/favicon.ico') }}">
+    <!-- Start CSS -->
+    <link href="{{ asset('assets/panel/horizontal/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/panel/horizontal/css/icons.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/panel/horizontal/css/flag-icon.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/panel/horizontal/css/style.css') }}" rel="stylesheet" type="text/css">
+    <!-- End CSS -->
+</head>
+<body class="vertical-layout">
+    <!-- Start Containerbar -->
+    <div id="containerbar" class="containerbar authenticate-bg">
+        <!-- Start Container -->
+        <div class="container">
+            @yield('content')
+        </div>
+        <!-- End Container -->
+    </div>
+    <!-- End Containerbar -->
+    <!-- Start JS -->
+    <script src="{{ asset('assets/panel/horizontal/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/panel/horizontal/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/panel/horizontal/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/panel/horizontal/js/modernizr.min.js') }}"></script>
+    <script src="{{ asset('assets/panel/horizontal/js/detect.js') }}"></script>
+    <script src="{{ asset('assets/panel/horizontal/js/jquery.slimscroll.js') }}"></script>
+    <!-- End js -->
+</body>
+</html>
