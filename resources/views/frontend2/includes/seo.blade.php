0e4146198af4c621985bf4e87979c59c953075c1
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-02-28 14:28:50 +0600

    frontend

--- resources/views/frontend2/includes/seo.blade.php
+++ resources/views/frontend2/includes/seo.blade.php
@@ -3,18 +3,19 @@
         <div class="row justify-content-center">
             <div class="col-lg-7 col-md-9 text-lg-left text-center">
                 <div class="section-title">
-                    <h5 class="subtitle"><span></span>Invest Now</h5>
-                    <h3 class="title">Get Featured!</h3>
-                    <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many</p>
+                    <h5 class="subtitle"><span></span>{{ get_static_option('who_we_are') }}</h5>
+                    <h3 class="title">{{ get_static_option('seo_highlight') }}</h3>
+                    <p>{{ get_static_option('seo_description') }}</p>
                 </div>
-                <div class="single-input-wrap">
-                    <input placeholder="Inter your email address" type="text" class="single-input">
-                    <a class="btn btn-basic" href="#">Start Invest</a>
-                </div>
+                @foreach($website_seos as $seo)
+                    <div class="single-input-wrap m-3">
+                        <input class="single-input" value="{{ $seo->text }}" readonly>
+                    </div>
+                @endforeach
             </div>
             <div class="col-lg-5 col-6 align-self-center banner-thumb-wrap">
                 <div class="thumb item-bounce">
-                    <img src="assets/img/feature/02.png" alt="img">
+                    <img src="{{ asset(get_static_option('seo_image') ?? get_static_option('no_image')) }}" alt="img">
                 </div>
             </div>
         </div>
