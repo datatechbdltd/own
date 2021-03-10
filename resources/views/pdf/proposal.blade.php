c74de09a232152c23aa116c8ae48b6662d75a240
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-03-10 16:06:57 +0600

    pdf

--- resources/views/pdf/proposal.blade.php
+++ resources/views/pdf/proposal.blade.php
@@ -1,41 +1,33 @@
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <link href="{{ asset('assets/pdf/proposal.css') }}" rel="stylesheet" type="text/css">
         <style>
 
-
-            .page-break {
-                page-break-after: always;
-            }
         </style>
-
     </head>
     <body>
-        {{-- <h1>Page 1</h1>
-        <div class="page-break"></div>
-        <h1>Page 2</h1> --}}
-        <table style="background-color: green; width:100%; text-align:center;">
-            <tr>
-                <td>
-                    <b style="font-size: 25px; color:black">DATATDCH BD LTD ডাতাটেক </b>
-                </td>
-            </tr>
-        </table>
-        <hr>
-        <table style="background-color: green; width:100%; height:23cm; text-align:center;">
-            <tr>
-                <td>
-                    <b style="font-size: 25px; color:black">DATATDCH BD LTD</b>
-                </td>
-            </tr>
-        </table>
-        <hr>
-        <table style="background-color: green; width:100%; text-align:center;">
-            <tr>
-                <td>
-                    <b style="font-size: 25px; color:black">DATATDCH BD LTD</b>
-                </td>
-            </tr>
-        </table>
+    <table style="background-color: green; width:100%; text-align:center;">
+        <tr>
+            <td>
+                <b style="font-size: 25px; color:black">DATATDCH BD LTD</b>
+            </td>
+        </tr>
+    </table>
+    <hr>
+    <table style="background-color: green; width:100%; height:11in; text-align:center;">
+        <tr>
+            <td>
+                <b style="font-size: 25px; color:black">DATATDCH BD LTD</b>
+            </td>
+        </tr>
+    </table>
+    <hr>
+    <table style="background-color: green; width:100%; text-align:center;">
+        <tr>
+            <td>
+                <b style="font-size: 25px; color:black">DATATDCH BD LTD</b>
+            </td>
+        </tr>
+    </table>
     </body>
 </html>
