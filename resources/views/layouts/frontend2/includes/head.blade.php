6fbe7a9d1c3bfa97cf82f9c14cf515599d7d3883
Ismath Jahan Jhumu <73467019+ismathjahanjhumu@users.noreply.github.com>
2021-03-13 17:29:48 +0600

    base url

--- resources/views/layouts/frontend2/includes/head.blade.php
+++ resources/views/layouts/frontend2/includes/head.blade.php
@@ -1,6 +1,7 @@
+<base href="{{ url('/') }}">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="keywords" content="{{ $meta_keywords  ?? get_static_option('meta_keywords')}}">
 <meta name="description" content="{!! $meta_description  ?? get_static_option('meta_description') !!}">
