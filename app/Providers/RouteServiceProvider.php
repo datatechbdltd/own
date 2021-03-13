dfe24a2ad90712386b7cf77ea9c3a60c55332e08
Ismath Jahan Jhumu <73467019+ismathjahanjhumu@users.noreply.github.com>
2021-03-14 00:04:54 +0600

    remove template

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -47,11 +47,6 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
-
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->prefix('template')
-                ->group(base_path('routes/template.php'));
         });
     }
 
